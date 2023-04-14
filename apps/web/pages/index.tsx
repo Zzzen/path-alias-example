21f7b774edbfc76d49313e8a897714a45145a199
Zzzen <callmetsing@gmail.com>
2023-04-14 18:53:41 +0800

    Intial Commit

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button, EMPTY_STRING } from "ui";
+
+export default function Web() {
+  return (
+    <div>
+      <h1>Web {EMPTY_STRING}</h1>
+      <Button />
+    </div>
+  );
+}
---
+++ packages/ui/index.tsx
@@ -0,0 +1,3 @@
+import * as React from "react";
+export * from "./Button";
+export { EMPTY_STRING } from '@utils/string'
